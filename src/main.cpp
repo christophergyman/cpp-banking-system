e3853f398eecfaa95e94313e0d0fb653245e5521
christophergyman <christophergayiuman@gmail.com>
2025-02-24 15:13:32 +0000

    update: formatting

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@ int main()
 {
     AccountHolder chrisHolder("Christopher", "Ga Yiu", "Man");
     AccountCard chrisCard(78882, 203070, 4, 12);
-
     Account christopherAccount(chrisHolder, chrisCard);
+
     christopherAccount.showAccountinformation();
 
     return 0;
